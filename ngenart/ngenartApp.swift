2987221d03c9f838c52f5dc4a0646c4facdf1ab1
pcybriwsky <pcybriwsky@gmail.com>
2024-08-16 21:44:29 -0400

    Initial Commit

---
+++ ngenart/ngenartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ngenartApp.swift
+//  ngenart
+//
+//  Created by Pedro on 8/16/24.
+//
+
+import SwiftUI
+
+@main
+struct ngenartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
